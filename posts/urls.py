8ac9454810178843253c5979c76af5fe453aa8c4
Arpit <15pa1a0511@vishnu.edu.in>
2018-06-24 17:24:40 +0530

    added post upload

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
-app_name = "posts"
-urlpatterns = [
 
+app_name = "posts"
+
+urlpatterns = [
+    path('add/',AddPost.as_view(),name="add_post")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from .forms import *
 
 # Create your views here.
+
+
+class AddPost(CreateView):
+    model = Posts
+    form_class = PostForm
+    template_name = "posts/create_post.html"
+
+    def post(self, request, *args, **kwargs):
+        post = PostForm(request.POST,request.FILES)
+        import ipdb
+        ipdb.set_trace()
+        if post.is_valid():
+            post = post.save(commit=False)
+            post.uploader = request.user
+            post.save()
+            return redirect('accounts:view_profile')
+        else:
+            return redirect("posts:add_post")
